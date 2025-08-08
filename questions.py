cf07a38fea56dfd808f67affe83dd01f596b9289
Gautam Kumar Choudhary <105611126+Gautamkr26@users.noreply.github.com>
2025-08-09 03:29:58 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,23 +1,128 @@
 bdi_questions = [
-    {"question": "1. Sadness", "options": ["I do not feel sad.", "I feel sad much of the time.", "I am sad all the time.", "I am so sad or unhappy that I can't stand it."]},
-    {"question": "2. Pessimism", "options": ["I am not discouraged about my future.", "I feel more discouraged about my future than I used to.", "I do not expect things to work out for me.", "I feel my future is hopeless and will only get worse."]},
-    {"question": "3. Past Failure", "options": ["I do not feel like a failure.", "I have failed more than I should have.", "As I look back, I see a lot of failures.", "I feel I am a total failure as a person."]},
-    {"question": "4. Loss of Pleasure", "options": ["I get as much pleasure as I ever did from the things I enjoy.", "I don't enjoy things as much as I used to.", "I get very little pleasure from the things I used to enjoy.", "I can't get any pleasure from the things I used to enjoy."]},
-    {"question": "5. Guilty Feelings", "options": ["I don't feel particularly guilty.", "I feel guilty over many things I have done or should have done.", "I feel quite guilty most of the time.", "I feel guilty all of the time."]},
-    {"question": "6. Punishment Feelings", "options": ["I don't feel I am being punished.", "I feel I may be punished.", "I expect to be punished.", "I feel I am being punished."]},
-    {"question": "7. Self-Dislike", "options": ["I feel the same about myself as ever.", "I have lost confidence in myself.", "I am disappointed in myself.", "I dislike myself."]},
-    {"question": "8. Self-Criticalness", "options": ["I don't criticize or blame myself more than usual.", "I am more critical of myself than I used to be.", "I criticize myself for all of my faults.", "I blame myself for everything bad that happens."]},
-    {"question": "9. Suicidal Thoughts", "options": ["I don't have any thoughts of killing myself.", "I have thoughts of killing myself, but I would not carry them out.", "I would like to kill myself.", "I would kill myself if I had the chance."]},
-    {"question": "10. Crying", "options": ["I don't cry anymore than I used to.", "I cry more than I used to.", "I cry over every little thing.", "I feel like crying but I can't."]},
-    {"question": "11. Agitation", "options": ["I am no more restless than usual.", "I feel more restless than usual.", "I am so restless it’s hard to sit still.", "I am so restless I have to keep moving."]},
-    {"question": "12. Loss of Interest", "options": ["I have not lost interest in other people or activities.", "I am less interested than I used to be.", "I have lost most of my interest.", "I have no interest at all."]},
-    {"question": "13. Indecisiveness", "options": ["I make decisions as well as ever.", "I find it harder to decide than usual.", "I have much greater difficulty in making decisions.", "I can’t make decisions at all."]},
-    {"question": "14. Worthlessness", "options": ["I do not feel worthless.", "I feel less valuable than others.", "I feel very worthless.", "I feel completely worthless."]},
-    {"question": "15. Loss of Energy", "options": ["I have as much energy as ever.", "I have less energy than I used to have.", "I don’t have enough energy to do very much.", "I don’t have enough energy to do anything."]},
-    {"question": "16. Sleeping Pattern", "options": ["I sleep as well as usual.", "I sleep a little more/less than usual.", "I sleep a lot more/less than usual.", "I sleep most of the day or wake up early and can’t go back to sleep."]},
-    {"question": "17. Irritability", "options": ["I am not more irritable than usual.", "I am more irritable than usual.", "I am much more irritable.", "I am irritable all the time."]},
-    {"question": "18. Appetite", "options": ["My appetite hasn’t changed.", "My appetite is somewhat increased/decreased.", "My appetite is much more/less than before.", "I have no appetite or eat constantly."]},
-    {"question": "19. Concentration", "options": ["I can concentrate well.", "I can’t concentrate as well.", "I find it hard to keep my mind on things.", "I can’t concentrate on anything."]},
-    {"question": "20. Fatigue", "options": ["I am not more tired than usual.", "I get tired more easily than before.", "I get tired doing simple tasks.", "I’m too tired to do anything."]},
-    {"question": "21. Interest in Sex", "options": ["No change in interest.", "Less interest than before.", "Much less interest.", "No interest at all."]}
+    {"question": "1. Sadness", "options": [
+        "I do not feel sad.",
+        "I feel sad much of the time.",
+        "I am sad all the time.",
+        "I am so sad or unhappy that I can't stand it."
+    ]},
+    {"question": "2. Pessimism", "options": [
+        "I am not discouraged about my future.",
+        "I feel more discouraged about my future than I used to.",
+        "I do not expect things to work out for me.",
+        "I feel my future is hopeless and will only get worse."
+    ]},
+    {"question": "3. Past Failure", "options": [
+        "I do not feel like a failure.",
+        "I have failed more than I should have.",
+        "As I look back, I see a lot of failures.",
+        "I feel I am a total failure as a person."
+    ]},
+    {"question": "4. Loss of Pleasure", "options": [
+        "I get as much pleasure as I ever did from the things I enjoy.",
+        "I don't enjoy things as much as I used to.",
+        "I get very little pleasure from the things I used to enjoy.",
+        "I can't get any pleasure from the things I used to enjoy."
+    ]},
+    {"question": "5. Guilty Feelings", "options": [
+        "I don't feel particularly guilty.",
+        "I feel guilty over many things I have done or should have done.",
+        "I feel quite guilty most of the time.",
+        "I feel guilty all of the time."
+    ]},
+    {"question": "6. Punishment Feelings", "options": [
+        "I don't feel I am being punished.",
+        "I feel I may be punished.",
+        "I expect to be punished.",
+        "I feel I am being punished."
+    ]},
+    {"question": "7. Self-Dislike", "options": [
+        "I feel the same about myself as ever.",
+        "I have lost confidence in myself.",
+        "I am disappointed in myself.",
+        "I dislike myself."
+    ]},
+    {"question": "8. Self-Criticalness", "options": [
+        "I don't criticize or blame myself more than usual.",
+        "I am more critical of myself than I used to be.",
+        "I criticize myself for all of my faults.",
+        "I blame myself for everything bad that happens."
+    ]},
+    {"question": "9. Suicidal Thoughts", "options": [
+        "I don't have any thoughts of killing myself.",
+        "I have thoughts of killing myself, but I would not carry them out.",
+        "I would like to kill myself.",
+        "I would kill myself if I had the chance."
+    ]},
+    {"question": "10. Crying", "options": [
+        "I don't cry anymore than I used to.",
+        "I cry more than I used to.",
+        "I cry over every little thing.",
+        "I feel like crying but I can't."
+    ]},
+    {"question": "11. Agitation", "options": [
+        "I am no more restless than usual.",
+        "I feel more restless than usual.",
+        "I am so restless it’s hard to sit still.",
+        "I am so restless I have to keep moving."
+    ]},
+    {"question": "12. Loss of Interest", "options": [
+        "I have not lost interest in other people or activities.",
+        "I am less interested than I used to be.",
+        "I have lost most of my interest.",
+        "I have no interest at all."
+    ]},
+    {"question": "13. Indecisiveness", "options": [
+        "I make decisions as well as ever.",
+        "I find it harder to decide than usual.",
+        "I have much greater difficulty in making decisions.",
+        "I can’t make decisions at all."
+    ]},
+    {"question": "14. Worthlessness", "options": [
+        "I do not feel worthless.",
+        "I feel less valuable than others.",
+        "I feel very worthless.",
+        "I feel completely worthless."
+    ]},
+    {"question": "15. Loss of Energy", "options": [
+        "I have as much energy as ever.",
+        "I have less energy than I used to have.",
+        "I don’t have enough energy to do very much.",
+        "I don’t have enough energy to do anything."
+    ]},
+    {"question": "16. Sleeping Pattern", "options": [
+        "I sleep as well as usual.",
+        "I sleep a little more/less than usual.",
+        "I sleep a lot more/less than usual.",
+        "I sleep most of the day or wake up early and can’t go back to sleep."
+    ]},
+    {"question": "17. Irritability", "options": [
+        "I am not more irritable than usual.",
+        "I am more irritable than usual.",
+        "I am much more irritable.",
+        "I am irritable all the time."
+    ]},
+    {"question": "18. Appetite", "options": [
+        "My appetite hasn’t changed.",
+        "My appetite is somewhat increased/decreased.",
+        "My appetite is much more/less than before.",
+        "I have no appetite or eat constantly."
+    ]},
+    {"question": "19. Concentration", "options": [
+        "I can concentrate well.",
+        "I can’t concentrate as well.",
+        "I find it hard to keep my mind on things.",
+        "I can’t concentrate on anything."
+    ]},
+    {"question": "20. Fatigue", "options": [
+        "I am not more tired than usual.",
+        "I get tired more easily than before.",
+        "I get tired doing simple tasks.",
+        "I’m too tired to do anything."
+    ]},
+    {"question": "21. Interest in Sex", "options": [
+        "No change in interest.",
+        "Less interest than before.",
+        "Much less interest.",
+        "No interest at all."
+    ]},
 ]
